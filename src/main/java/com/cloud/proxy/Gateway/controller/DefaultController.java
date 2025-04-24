db253ae9e17bf815551e76963f0ebc205cf3195a
JuanesOsorioL <juanchis_1989@hotmail.com>
2025-04-23 19:18:57 -0500

    verificar cors

--- src/main/java/com/cloud/proxy/Gateway/controller/DefaultController.java
+++ src/main/java/com/cloud/proxy/Gateway/controller/DefaultController.java
@@ -2,12 +2,19 @@ package com.cloud.proxy.Gateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {
+        "http://127.0.0.1:5501",
+        "http://localhost:5501",
+        "http://localhost:8282/api/productos",
+        "http://localhost:8282"
+})
 public class DefaultController {
 
     @RequestMapping("/default")
